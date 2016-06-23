cf02b5f48cf6bb7182ba9660eaae82d19539bd69
Julius Peinelt <julius.peinelt@mailbox.org>
2016-06-23 22:38:49 +0200

    mini suave with some routes

--- MiniSuave/Program.fs
+++ MiniSuave/Program.fs
@@ -1,7 +1,35 @@
-﻿// Weitere Informationen zu F# unter "http://fsharp.org".
-// Weitere Hilfe finden Sie im Projekt "F#-Lernprogramm".
+﻿#if INTERACTIVE
+#load "Suave.fs"
+#else
+module MiniSuaveIO
+#endif
+open Suave.Http
+open Suave.Console
+open Suave.Successful
+open Suave.Combinators
+open Suave.Filters
 
+let private main argv = 
+    let request = {Route = ""; Type = Suave.Http.GET}
+    let response = {Content = ""; StatusCode = 200}
+    let context = {Request = request; Response = response}
+
+    let app = Choose [
+                GET >=> Path "/hello" >=> OK "Hello GET"
+                POST >=> Path "/hello" >=> OK "Hello POST"
+                Path "/foo" >=> Choose [
+                                    GET >=> OK "Foo GET"
+                                    POST >=> OK "Foo POST"
+                                ]
+                ]
+
+    executeInLoop context app
+    System.Console.ReadKey() |> ignore
+    0
+
+#if INTERACTIVE
+fsi.CommandLineArgs |> Array.toList |> List.tail |> List.toArray |> main
+#else
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // Exitcode aus ganzen Zahlen zurückgeben
+let entryPoint args = main args
+#endif
